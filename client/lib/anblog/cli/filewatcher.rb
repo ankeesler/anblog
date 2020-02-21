f6eec8a98f097e2f0da182e7a38da48254f5af55
Andrew Keesler <ankeesler1@gmail.com>
2020-02-20 19:13:55 -0500

    Update filewatcher. Keep it simple.

--- client/lib/anblog/cli/filewatcher.rb
+++ client/lib/anblog/cli/filewatcher.rb
@@ -3,35 +3,16 @@ require 'filewatcher'
 module Anblog
   module CLI
     class Filewatcher
-      def initialize(file, callbacks)
-        @file = file
-        @callbacks = callbacks
-      end
-
-      def start
-        @fw = ::Filewatcher.new [@file]
+      def start(file, &block)
+        @fw = ::Filewatcher.new [file]
         @thread = Thread.new {
-          @fw.watch { |f, e| callback(f, e) }
+          @fw.watch block
         }
       end
 
       def stop
         @fw.stop
-        @fw.finalize { |f, e| callback(f, e) }
         @thread.join
-      end
-
-      private
-
-      def callback(f, e)
-        case e
-        when :created
-          @callbacks.on_create f
-        when :updated
-          @callbacks.on_update f
-        when :deleted
-          @callbacks.on_delete f
-        end
       end
     end
   end
